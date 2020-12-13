5504b7f63c83969015f4f873de64017cdd655c99
ZakobG <59724611+ZakobG@users.noreply.github.com>
2020-12-13 13:52:45 +0100

    Initial commit

---
+++ src/User.java
@@ -0,0 +1,21 @@
+public class User {
+    private String firstname;
+    private String lastname;
+    private int age;
+    private String username;
+    private String password;
+    private String gender;
+    private int phone;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public User getUser() {
+        return getUser();
+    }
+}
---
+++ src/UserBuilder.java
@@ -0,0 +1,9 @@
+public class UserBuilder {
+    public static User createUser(){
+        User kwk = new User();
+        kwk.setUsername("KiwiK");
+        kwk.setPassword("Trebaheslo");
+
+        return kwk.getUser();
+    }
+}
